9c48ca20778b1c4962f1195ceccaea5ed137b15d
jhuayhuav <62222046+jhuayhuav@users.noreply.github.com>
2022-04-28 10:52:50 -0500

    jhuayhuav 28.04.2022 api gateway

--- src/main/java/com/bootcamp/apigateway/MsApiGateway.java
+++ src/main/java/com/bootcamp/apigateway/MsApiGateway.java
@@ -2,12 +2,14 @@ package com.bootcamp.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class DemoApplication {
+public class MsApiGateway {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MsApiGateway.class, args);
 	}
 
 }
